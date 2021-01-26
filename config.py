c3374880364956a69e054f11b08d10552d4317dc
DanielJAM <daniel.maaskant@gmail.com>
2021-01-26 16:20:31 +0100

    Added my name, interpolate -> upsample for torch 0.4.0

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ Base Configurations class.
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
+Edited by Daniel Maaskant
 """
 
 import math
